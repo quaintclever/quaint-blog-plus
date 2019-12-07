36e15828a334f7f30313d6b78230c976cd1e10d9
quaint <qicong@ilarge.cn>
2019-12-07 20:31:53 +0800

    基本数据提交

--- src/main/resources/sql/dml/member_data.sql
+++ src/main/resources/sql/dml/member_data.sql
@@ -1,3 +1,4 @@
+-- 用户测试数据
 INSERT INTO `quaint_blog_plus`.`member_info`(`id`, `nack_name`) VALUES (1, 'quaint');
 INSERT INTO `quaint_blog_plus`.`member_info`(`id`, `nack_name`) VALUES (2, 'Visitor');
 INSERT INTO `quaint_blog_plus`.`member_info`(`id`, `nack_name`) VALUES (3, '果冻');
